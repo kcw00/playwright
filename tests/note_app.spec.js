e7f7e68851cc68736eae3b0d9ff0ef6d1a7600cd
chaewon kim <cheawon0319@gmail.com>
2025-02-10 14:55:40 -0800

    isolate the common part in the beforeEach()

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -1,16 +1,17 @@
-const { test, expect, describe } = require('@playwright/test')
+const { test, expect, describe, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
-
+  })
+  
+  test('front page can be opened', async ({ page }) => {
     const locator = await page.getByText('Notes')
     await expect(locator).toBeVisible()
     await expect(page.getByText('Made by React')).toBeVisible()
   })
 
   test('login form can be opened', async({ page }) => {
-    await page.goto('http://localhost:5173')
     await page.getByRole('button', { name: 'log in '}).click()
     await page.getByTestId('username').fill('chaewon')
     await page.getByTestId('password').fill('thisispw')
