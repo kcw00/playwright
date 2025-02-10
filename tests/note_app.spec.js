f9cce7a57d87c346d8bef291220136338f188e34
chaewon kim <cheawon0319@gmail.com>
2025-02-10 14:50:43 -0800

    testing for logging in

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -6,6 +6,17 @@ describe('Note app', () => {
 
     const locator = await page.getByText('Notes')
     await expect(locator).toBeVisible()
-    await expect(page.getByText('Notes, Made by React')).toBeVisible()
+    await expect(page.getByText('Made by React')).toBeVisible()
   })
+
+  test('login form can be opened', async({ page }) => {
+    await page.goto('http://localhost:5173')
+    await page.getByRole('button', { name: 'log in '}).click()
+    await page.getByRole('textbox').first().fill('chaewon')
+    await page.getByRole('textbox').last().fill('thisispw')
+    await page.getByRole('button', { name: 'login'}).click()
+
+    await expect(page.getByText('Chaewon Kim logged in')).toBeVisible()
+  })
+
 })
