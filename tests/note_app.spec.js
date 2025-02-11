de753bd5333964c479e6e25cbf63f88fbfb5ac95
chaewon kim <cheawon0319@gmail.com>
2025-02-10 16:03:46 -0800

    testing for failed login

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -20,39 +20,53 @@ describe('Note app', () => {
     await expect(page.getByText('Made by React')).toBeVisible()
   })
 
-  test('login form can be opened', async({ page }) => {
-    await page.getByRole('button', { name: 'log in '}).click()
+  test('login form can be opened', async ({ page }) => {
+    await page.getByRole('button', { name: 'log in ' }).click()
     await page.getByTestId('username').fill('chaewon')
     await page.getByTestId('password').fill('thisispw')
-    await page.getByRole('button', { name: 'login'}).click()
+    await page.getByRole('button', { name: 'login' }).click()
 
     await expect(page.getByText('Chaewon Kim logged in')).toBeVisible()
   })
 
+  test('login fails with wrong password', async ({ page }) => {
+    await page.getByRole('button', { name: 'log in ' }).click()
+    await page.getByTestId('username').fill('chaewon')
+    await page.getByTestId('password').fill('wrong')
+    await page.getByRole('button', { name: 'login ' }).click()
+
+    const errorDiv = await page.locator('.error')
+    await expect(errorDiv).toContainText('Wrong credentials')
+    await expect(errorDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+
+    await expect(page.getByText('Chaewon Kim logged in')).not.toBeVisible()
+  })
+
   describe('when logged in', () => {
-    beforeEach(async({page}) => {
-      await page.getByRole('button', {name: 'log in'}).click()
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'log in' }).click()
       await page.getByTestId('username').fill('chaewon')
       await page.getByTestId('password').fill('thisispw')
-      await page.getByRole('button', { name: 'login'}).click()
+      await page.getByRole('button', { name: 'login' }).click()
     })
 
-    test('a new note can be created', async({ page }) => {
-      await page.getByRole('button', {name: 'new note'}).click()
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
       await page.getByRole('textbox').fill('a note created by playwright')
-      await page.getByRole('button', {name: 'save'}).click()
+      await page.getByRole('button', { name: 'save' }).click()
       await expect(page.getByText('a note created by playwright')).toBeVisible()
     })
 
     describe('and a note exists', () => {
-      beforeEach(async ({page}) => {
-        await page.getByRole('button', { name: 'new note'}).click()
+      beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'new note' }).click()
         await page.getByRole('textbox').fill('another note by playwright')
         await page.getByRole('button', { name: 'save' }).click()
       })
 
-      test('importance can be changed', async({ page }) => {
-        await page.getByRole('button', { name: 'make not important '}).click()
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important ' }).click()
         await expect(page.getByText('make important')).toBeVisible()
       })
     })
